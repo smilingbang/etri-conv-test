45eacf1fd977c8b085821645c78e8aabb4c112df
ebk <smilingbang@gmail.com>
2019-04-08 03:33:11 +0900

    conv_diy.cpp

--- conv_diy.cpp
+++ conv_diy.cpp
@@ -12,15 +12,114 @@ int your_conv( cv::Mat src,
     int src_height = src.rows;
     int src_width = src.cols;
 
-    int kernel_height = kernel.rows;
-    int kernel_width = kernel.cols;
+    //int kernel_height = kernel.rows;
+    //int kernel_width = kernel.cols;
 
-    int dst_height = ;
-    int dst_width = ;
+    //int dst_height = ;
+    //int dst_width = ;
 
     // src.ptr<unsigned char>(i)[ calculate INDEX ]
 
     // MAKE YOUR OWN CONVOLUTION PROCESS
+    int k;//Number of Channel
+    int i,j;//i : Width   j : Height
+
+
+
+    for(k=0;k<3;k++){
+	for (i=0;i<src_width;i=i+stride){
+		for (j=0;j<src_height;j=j+stride){
+			
+		    if(i/=0&&j/=0){
+			 dst.at<cv::vec3b>(i,j)[k]=src.at<cv::vec3b>(i-1,j-1)[k]*kernel.at<uchar>(0,0)[k]
+				                  +src.at<cv::vec3b>(i-1,j)[k]*kernel.at<uchar>(0,1)[k]
+						  +src.at<cv::vec3b>(i-1,j+1)[k]*kernel.at<uchar>(0,2)[k]
+						  +src.at<cv::vec3b>(i,j-1)[k]*kernel.at<uchar>(1,0)[k]
+						  +src.at<cv::vec3b>(i,j)[k]*kernel.at<uchar>(1,1)[k]
+						  +src.at<cv::vec3b>(i,j+1)[k]*kernel.at<uchar>(1,2)[k]
+						  +src.at<cv::vec3b>(i+1,j-1)[k]*kernel.at<uchar>(2,0)[k]
+						  +src.at<cv::vec3b>(i+1,j)[k]*kernel.at<uchar>(2,1)[k]
+						  +src.at<cv::vec3b>(i+1,j+1)[k]*kernel.at<uchar>(2,2)[k];
+		    }
+		    else if(i==0 && j==0){
+		        //i-1==0 j-1=0
+			  dst.at<cv::vec3b>(i,j)[k]=padding*kernel.at<uchar>(0,0)[k]
+			                           +padding*kernel.at<uchar>(0,1)[k]
+				                   +padding*kernel.at<uchar>(0,2)[k]
+				                   +padding*kernel.at<uchar>(1,0)[k]
+				                   +src.at<cv::vec3b>(i,j)[k]*kernel.at<uchar>(1,1)[k]
+				                   +src.at<cv::vec3b>(i,j+1)[k]*kernel.at<uchar>(1,2)[k]
+				                   +padding*kernel.at<uchar>(2,0)[k]
+				                   +src.at<cv::vec3b>(i+1,j)[k]*kernel.at<uchar>(2,1)[k] 	                                                         +src.at<cv::vec3b>(i+1,j+1)[k]*kernel.at<uchar>(2,2)[k];
+		    } 
+		    else if(i==0){
+			//i-1==0
+			   dst.at<cv::vec3b>(i,j)[k]=padding*kernel.at<uchar>(0,0)[k]
+			                            +padding*kernel.at<uchar>(0,1)[k]
+			                            +padding*kernel.at<uchar>(0,2)[k]
+			                            +src.at<cv::vec3b>(i,j-1)[k]*kernel.at<uchar>(1,0)[k]
+			                            +src.at<cv::vec3b>(i,j)[k]*kernel.at<uchar>(1,1)[k]
+			                            +src.at<cv::vec3b>(i,j+1)[k]*kernel.at<uchar>(1,2)[k]
+			                            +src.at<cv::vec3b>(i+1,j-1)[k]*kernel.at<uchar>(2,0)[k]
+			                            +src.at<cv::vec3b>(i+1,j)[k]*kernel.at<uchar>(2,1)[k]
+			                            +src.at<cv::vec3b>(i+1,j+1)[k]*kernel.at<uchar>(2,2)[k];
+			
+		    }
+		    else if(j==0){
+			//j-1==0
+		           dst.at<cv::vec3b>(i,j)[k]=padding*kernel.at<uchar>(0,0)[k]
+			                            +src.at<cv::vec3b>(i-1,j)[k]*kernel.at<uchar>(0,1)[k]
+			                            +src.at<cv::vec3b>(i-1,j+1)[k]*kernel.at<uchar>(0,2)[k]
+			                            +padding*kernel.at<uchar>(1,0)[k]
+			                            +src.at<cv::vec3b>(i,j)[k]*kernel.at<uchar>(1,1)[k]
+			                            +src.at<cv::vec3b>(i,j+1)[k]*kernel.at<uchar>(1,2)[k]
+			                            +padding*kernel.at<uchar>(2,0)[k]
+			                            +src.at<cv::vec3b>(i+1,j)[k]*kernel.at<uchar>(2,1)[k]
+			                            +src.at<cv::vec3b>(i+1,j+1)[k]*kernel.at<uchar>(2,2)[k];
+
+		    }
+		    else if(i==src_width-1&&j==src_height-1){//i+1=0 j+1=0
+			   dst.at<cv::vec3b>(i,j)[k]=src.at<cv::vec3b>(i-1,j-1)[k]*kernel.at<uchar>(0,0)[k]
+			                            +src.at<cv::vec3b>(i-1,j)[k]*kernel.at<uchar>(0,1)[k]
+			                            +padding*kernel.at<uchar>(0,2)[k]
+			                            +src.at<cv::vec3b>(i,j-1)[k]*kernel.at<uchar>(1,0)[k]
+			                            +src.at<cv::vec3b>(i,j)[k]*kernel.at<uchar>(1,1)[k]
+			                            +padding*kernel.at<uchar>(1,2)[k]
+			                            +padding*kernel.at<uchar>(2,0)[k]
+			                            +padding*kernel.at<uchar>(2,1)[k]
+			                            +padding*kernel.at<uchar>(2,2)[k];
+		    }
+		    else if(i==src_width-1){//i+1=0
+		           dst.at<cv::vec3b>(i,j)[k]=src.at<cv::vec3b>(i-1,j-1)[k]*kernel.at<uchar>(0,0)[k]
+			                            +src.at<cv::vec3b>(i-1,j)[k]*kernel.at<uchar>(0,1)[k]
+			                            +src.at<cv::vec3b>(i-1,j+1)[k]*kernel.at<uchar>(0,2)[k]
+			                            +src.at<cv::vec3b>(i,j-1)[k]*kernel.at<uchar>(1,0)[k]
+			                            +src.at<cv::vec3b>(i,j)[k]*kernel.at<uchar>(1,1)[k]
+			                            +src.at<cv::vec3b>(i,j+1)[k]*kernel.at<uchar>(1,2)[k]
+			                            +padding*kernel.at<uchar>(2,0)[k]
+			                            +padding*kernel.at<uchar>(2,1)[k]
+			                            +padding*kernel.at<uchar>(2,2)[k];
+		    }
+		    else {//j+1=0
+                         dst.at<cv::vec3b>(i,j)[k]=src.at<cv::vec3b>(i-1,j-1)[k]*kernel.at<uchar>(0,0)[k]
+				                  +src.at<cv::vec3b>(i-1,j)[k]*kernel.at<uchar>(0,1)[k]
+					          +padding*kernel.at<uchar>(0,2)[k]
+					          +src.at<cv::vec3b>(i,j-1)[k]*kernel.at<uchar>(1,0)[k]
+					          +src.at<cv::vec3b>(i,j)[k]*kernel.at<uchar>(1,1)[k] 
+					          +padding*kernel.at<uchar>(1,2)[k]	
+						  +src.at<cv::vec3b>(i+1,j-1)[k]*kernel.at<uchar>(2,0)[k]
+						  +src.at<cv::vec3b>(i+1,j)[k]*kernel.at<uchar>(2,1)[k]	
+						  +padding*kernel.at<uchar>(2,2)[k];
+		    }
+		}
+	}
+    }
+	
+
+
+
+
+
 
     // if success
     return 0
